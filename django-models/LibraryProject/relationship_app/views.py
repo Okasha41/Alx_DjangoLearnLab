e53583e3ca6e4e1a96c43c96e40be37e0a45e398
AbdulRahman Okasha <a.majdy41@gmail.com>
2025-03-19 00:43:15 +0200

    update the register form

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -68,7 +68,7 @@ class LogoutView(LogoutView):
 
 
 class register(FormView):
-    template_name = 'register.html'
+    template_name = 'relationship_app/register.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
 
