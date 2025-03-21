ade72e813500e9c7429f035f825e1fb5a55528e4
AbdulRahman Okasha <a.majdy41@gmail.com>
2025-03-21 02:19:14 +0200

    created the advance_api_project

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 
 
 class Author(models.Model):
@@ -70,3 +71,18 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class CustomeUserManager(AbstractBaseUser):
+    def create_user(self):
+        pass
+
+    def create_superuser(self):
+        pass
+
+
+class CustomeUserModel(AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
+
+    objects = CustomeUserManager()
