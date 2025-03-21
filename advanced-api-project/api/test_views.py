f97b17bad794e6f755f40a52877de8e6c200a4f3
AbdulRahman Okasha <a.majdy41@gmail.com>
2025-03-21 05:30:53 +0200

    update test cases

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
+from rest_framework import status
 from .models import Book, Author
+
+# response.data
 
 
 class BookTestCase(TestCase):
