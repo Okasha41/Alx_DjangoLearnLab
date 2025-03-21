77924e6095fb7765484cf2ba82dd8a145d2bfcff
AbdulRahman Okasha <a.majdy41@gmail.com>
2025-03-21 04:16:02 +0200

    update the urls in api app

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('/books/', views.ListView()),
-    path('books/<int:pk>'), views.DetailView()
+    path('books/<int:pk>', views.DetailView()),
+    path('/books/create/', views.CreateView()),
+    path('/books/update/', views.UpdateView()),
+    path('books/delete/', views.DeleteView())
 ]
