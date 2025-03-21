2c687634dcf922974b2e3ee1645b645a4fbc9c01
AbdulRahman Okasha <a.majdy41@gmail.com>
2025-03-21 04:22:06 +0200

    add IsAuthenticatedOrReadOnly to import

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 
