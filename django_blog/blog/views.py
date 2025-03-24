fe3bd71bc484784bfdc494f9eba78f34be5c9532
AbdulRahman Okasha <a.majdy41@gmail.com>
2025-03-24 04:00:27 +0200

    update the blog app

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.http import HttpResponse
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.models import auth
+
+from .forms import LoginForm, CreateUserForm
+
+
+def home(request):
+    return render(request=request, template_name='blog/base.html')
+
+
+def posts(request):
+    pass
+
+
+def login(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('home')
+    context = {'loginform': form}
+    return render(request, 'blog/login.html', context=context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
+
+def register(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    context = {'registerform': form}
+    return render(request, 'blog/register.html', context=context)
+
+
+def profile(request):
+    return HttpResponse('this is profile page')
