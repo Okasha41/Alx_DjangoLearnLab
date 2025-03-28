fa10faae602c3855e467f54d0ec57cb7d48f15ab
AbdulRahman Okasha <a.majdy41@gmail.com>
2025-03-28 02:27:14 +0200

    update urls

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,6 +1,8 @@
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
 
+
+# login/ // register/
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='user')
 
